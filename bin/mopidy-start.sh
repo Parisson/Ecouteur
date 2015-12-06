24b40c8085a6a02bc80ce172eb916cbe3133f909
Guillaume Pellerin <yomguy@parisson.com>
2015-12-06 14:48:11 +0100

    bugfix day detection, simplify crontab

--- bin/mopidy-start.sh
+++ bin/mopidy-start.sh
@@ -6,7 +6,7 @@ DOW=$(date +%u)
 
 declare -A playlists
 
-playlists=([1]="lundi" [2]="mardi" [3]="mercredi" [4]="jeudi" [5]="vendredi" [6]="samedi" [0]="dimanche")
+playlists=([1]="lundi" [2]="mardi" [3]="mercredi" [4]="jeudi" [5]="vendredi" [6]="samedi" [7]="dimanche")
 
 playlist=${playlists[$DOW]}
 
